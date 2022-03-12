9c15eb46d343af8d28034b62466e053e7e71b0a1
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-03-12 10:01:23 +0300

    fixed

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,11 +4,11 @@ const Error401 = require('../utils/errors/Error401');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith('Bearer')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new Error401('Необходима авторизация'));
   }
 
-  const token = authorization.replace('Bearer', '');
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
