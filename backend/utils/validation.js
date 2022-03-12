ac01e464748831b74906a50c1e1e626584152f83
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-03-12 19:24:08 +0300

    fixed: validationjs28

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -25,7 +25,8 @@ const validateAvatar = celebrate({
 const validateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().pattern(/[-a-zA-Z0-9@:%_.~#?&=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_.~#?&=]*)?/),
+    //link: Joi.string().required().pattern(/[-a-zA-Z0-9@:%_.~#?&=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_.~#?&=]*)?/),
+    link: Joi.string().required().pattern(/https?:\/\/(www)?[-.~:/?#[\]@!$&'()*+,;=\w]+#?\b/),
   }),
 });
 
